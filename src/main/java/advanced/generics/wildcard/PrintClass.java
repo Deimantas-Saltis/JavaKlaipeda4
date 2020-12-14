d353016822df8210cbb25ca3d7e6e8b650c0941b
DeimantasS <deimantas.saltis@gmail.com>
2020-12-14 11:22:50 +0200

    add examples of generics with wildcard

---
+++ src/main/java/advanced/generics/wildcard/PrintClass.java
@@ -0,0 +1,22 @@
+package advanced.generics.wildcard;
+
+import java.util.Collection;
+
+public class PrintClass {
+
+    // Example with generic
+    public static <T> void printCollectionG(Collection<T> collection) {
+        for (T element : collection) {
+            System.out.printf("%s ", element);
+        }
+        System.out.println();
+    }
+
+    // Example with wildcard
+    public static void printCollectionW(Collection<?> collection) {
+        for (Object element : collection) {
+            System.out.printf("%s ", element);
+        }
+        System.out.println();
+    }
+}
